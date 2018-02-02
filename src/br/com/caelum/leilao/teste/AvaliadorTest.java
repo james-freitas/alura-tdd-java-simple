3b2dfb0521fa29d8e816119611defba3a686fb21
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-02-02 07:31:31 -0200

    Matematica maluca

--- src/br/com/caelum/leilao/teste/AvaliadorTest.java
+++ src/br/com/caelum/leilao/teste/AvaliadorTest.java
@@ -106,26 +106,60 @@ public class AvaliadorTest {
 	
 	*/
 	@Test
-	public void deveEncontrarOsTresMaioresLances(){
-		Usuario joao = new Usuario("João");
-		Usuario maria = new Usuario("Maria");
-		
-		Leilao leilao = new Leilao("Playstation 4 Novo");
-		
-		leilao.propoe(new Lance(joao, 100.0));
-		leilao.propoe(new Lance(maria, 200.0));
-		leilao.propoe(new Lance(joao, 300.0));
-		leilao.propoe(new Lance(maria, 400.0));
-		
-		Avaliador leiloeiro = new Avaliador();
-		leiloeiro.avalia(leilao);
-		
-		List<Lance> maiores = leiloeiro.getTresMaiores();
-		assertEquals(3, maiores.size());
-		assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
-		assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
-		assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
-	}
+    public void deveEncontrarOsTresMaioresLances() {
+        Usuario joao = new Usuario("João");
+        Usuario maria = new Usuario("Maria");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao, 100.0));
+        leilao.propoe(new Lance(maria, 200.0));
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(maria, 400.0));
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        List<Lance> maiores = leiloeiro.getTresMaiores();
+
+        assertEquals(3, maiores.size());
+        assertEquals(400, maiores.get(0).getValor(), 0.00001);
+        assertEquals(300, maiores.get(1).getValor(), 0.00001);
+        assertEquals(200, maiores.get(2).getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveDevolverTodosLancesCasoNaoHajaNoMinimo3() {
+        Usuario joao = new Usuario("João");
+        Usuario maria = new Usuario("Maria");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao, 100.0));
+        leilao.propoe(new Lance(maria, 200.0));
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        List<Lance> maiores = leiloeiro.getTresMaiores();
+
+        assertEquals(2, maiores.size());
+        assertEquals(200, maiores.get(0).getValor(), 0.00001);
+        assertEquals(100, maiores.get(1).getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveDevolverListaVaziaCasoNaoHajaLances() {
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        List<Lance> maiores = leiloeiro.getTresMaiores();
+
+        assertEquals(0, maiores.size());
+    }
+	
+	
+
 	
 	@Test
 	public void deveEntenderLancesEmOrdemAleatoria(){
---
+++ src/br/com/caelum/matematica/teste/MatematicaMalucaTest.java
@@ -0,0 +1,36 @@
+package br.com.caelum.matematica.teste;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.caelum.matematica.MatematicaMaluca;
+
+public class MatematicaMalucaTest {
+/*
+	Um para o caso onde o número é maior que 30; outro para o caso onde o número é maior que 10,
+	 mas menor que 30; outro para o caso onde o número é menor que 10.
+
+	Podemos ainda aumentar nossa segurança, e testar o que acontece quando passamos exatamente os 
+	números 10 e 30, afinal são casos extremos e merecem ser testados.
+	
+*/	
+    @Test
+    public void deveMultiplicarNumerosMaioresQue30() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(50*4, matematica.contaMaluca(50));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMaioresQue10EMenoresQue30() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(20*3, matematica.contaMaluca(20));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMenoresQue10() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(5*2, matematica.contaMaluca(5));
+    }
+	
+}
