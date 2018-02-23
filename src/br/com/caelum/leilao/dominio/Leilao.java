bd62ab0cbd09c1672be7ffbe2b085c93623de31f
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-02-23 08:23:32 -0300

    Testing with TDD

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -15,7 +15,30 @@ public class Leilao {
 	}
 	
 	public void propoe(Lance lance) {
-		lances.add(lance);
+		
+
+		
+		if(lances.isEmpty() || podeDarLance(lance.getUsuario())){
+			lances.add(lance);
+		}
+		
+	}
+
+	private boolean podeDarLance(Usuario usuario){
+		return !ultimoLanceDado().getUsuario().equals(usuario) 
+	            && qtdDelancesDo(usuario) < 5;
+	}
+	
+	private Lance ultimoLanceDado(){
+		return lances.get(lances.size() - 1);
+	}
+	
+	private int qtdDelancesDo(Usuario usuario) {
+		int total = 0;
+		for (Lance lance : lances) {
+			if(lance.getUsuario().equals(usuario)) total++;
+		}
+		return total;
 	}
 
 	public String getDescricao() {
@@ -25,6 +48,7 @@ public class Leilao {
 	public List<Lance> getLances() {
 		return Collections.unmodifiableList(lances);
 	}
+
 
 	
 	
